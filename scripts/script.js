c9022cb4c2be25b1f496803c122910dc9d12f4df
FernandoHipolito <fernandofhipolito@gmail.com>
2024-02-28 20:27:47 -0300

    create popup card html e javascript

--- scripts/script.js
+++ scripts/script.js
@@ -6,6 +6,7 @@ const editProfilePopupElement = document.querySelector(".popup");
 const editProfileCloseButtonElement = document.querySelector(
   ".popup__close-button"
 );
+
 const likeButtonElements = document.querySelectorAll(".elements__like-button");
 
 function handleProfileFormSubmit(evt) {
@@ -53,12 +54,61 @@ function closeEditProfilePopup() {
   editProfileFormElement.removeEventListener("submit", handleProfileFormSubmit);
 }
 
+const editCardButtonElement = document.querySelector(".profile__addbutton");
+const editCardFormElement = document.querySelector(".popup__form-card");
+const editCardPopupElement = document.querySelector(".popup__card");
+const editCardCloseButtonElement = document.querySelector(
+  ".close__card-button"
+);
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+
+  const titleInput = document.querySelector("#title");
+  const urlInput = document.querySelector("#url");
+
+  const newTitle = titleInput.value;
+  const newUrl = urlInput.value;
+
+  const titleInputCurrent = document.querySelector(".elements__title");
+  const urlInputCurrent = document.querySelector(".elements__image");
+
+  titleInputCurrent.textContent = newTitle;
+  urlInputCurrent.textContent = newUrl;
+  closeEditCardPopup();
+}
+
+function openEditCardPopup() {
+  const titleInput = document.querySelector("#title");
+  const urlInput = document.querySelector("#url");
+
+  const titleInputCurrent = document.querySelector(".elements__title");
+  const urlInputCurrent = document.querySelector(".elements__image");
+
+  titleInput.value = titleInputCurrent.textContent;
+  urlInput.value = urlInputCurrent.textContent;
+
+  editCardPopupElement.classList.add("popup_opened");
+
+  editCardFormElement.addEventListener("submit", handleCardFormSubmit);
+  editCardCloseButtonElement.addEventListener("click", closeEditCardPopup);
+}
+
+function closeEditCardPopup() {
+  editCardPopupElement.classList.remove("popup_opened");
+  editCardCloseButtonElement.removeEventListener("click", closeEditCardPopup);
+  editCardFormElement.removeEventListener("submit", handleCardFormSubmit);
+}
+
 function toggleLikeDislike(evt) {
   const likeButtonClickedElement = evt.target;
   likeButtonClickedElement.classList.toggle("active");
 }
 
 editProfileButtonElement.addEventListener("click", openEditProfilePopup);
+
+editCardButtonElement.addEventListener("click", openEditCardPopup);
+
 likeButtonElements.forEach((el) =>
   el.addEventListener("click", toggleLikeDislike)
 );
