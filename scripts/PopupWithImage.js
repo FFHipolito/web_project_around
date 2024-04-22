67f36b8461770090b397fd3a053f549a06be4911
FernandoHipolito <fernandofhipolito@gmail.com>
2024-04-22 14:30:27 -0300

    create PopupWithImage class

---
+++ scripts/PopupWithImage.js
@@ -0,0 +1,18 @@
+import Popup from "./Popup.js";
+
+export default class PopupWithImage extends Popup {
+  constructor(popupSelector) {
+    super(popupSelector);
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open(data) {
+    super.open();
+
+    const imageTitle = this._popupElement.querySelector(".popup__image-name");
+    const image = this._popupElement.querySelector(".popup__image");
+    imageTitle.textContent = data.name;
+    image.src = data.link;
+    image.alt = data.name;
+  }
+}
