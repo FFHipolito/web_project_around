65a7a85c0b7faf50a71c79a98ef33d4237c22064
FernandoHipolito <fernandofhipolito@gmail.com>
2024-04-22 13:55:16 -0300

    create Popup class

---
+++ scripts/Popup.js
@@ -0,0 +1,26 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.remove("popup_opened");
+    this._popup.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    this._popupElement.classList.remove("popup_opened");
+    this._popup.addEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key == "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    const closeButton = this._popup.querySelector(".popup__close-button");
+    closeButton.addEventListener("click", () => this.close());
+  }
+}
